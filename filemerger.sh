1c5907aeca1a9479fd70e8b93ac1a2417f8bcd9d
he <he@nykredit.dk>
2021-04-29 18:50:29 +0200

    update filemerger.sh

--- filemerger.sh
+++ filemerger.sh
@@ -12,8 +12,8 @@ out_file=$out_dir/Out.$file_type
 # clear out_file
 rm -rf "$out_file"
 
-# copy all import statements first
-find "$src_path" ! -path '*/test/*' -type f -name "*.$file_type" -exec grep -E 'import' {} \; > "$out_file"
+# copy all import statements first and Remove duplicated imports
+find "$src_path" ! -path '*/test/*' -type f -name "*.$file_type" -exec grep -E 'import' {} \; | sort | uniq > "$out_file"
 
 # copy everything else excluding package and import statements
-find "$src_path" ! -path '*/test/*' -type f -name "*.$file_type" -exec grep -Ev 'package|import' {} \; | sed -e 's/^public class/class/' >> "$out_file"
+find "$src_path" ! -path '*/test/*' -type f -name "*.$file_type" -exec grep -Ev 'package|import' {} \; | sed -e 's/^public class/class/' | sed -e 's/^public enum/enum/' >> "$out_file"
